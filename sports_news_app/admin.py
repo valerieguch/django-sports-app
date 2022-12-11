51d77884e1620117985753bc8fcc4920b5d31c03
Valerieguch <valeria.guch2000@gmail.com>
2022-12-11 18:34:36 +0300

    Add `Tag`, `Author` models, reset migrations because I messed up

--- sports_news_app/admin.py
+++ sports_news_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Author, Tag, Article
 
-# Register your models here.
+# admin.site.register(Article)
+
+@admin.register(Author)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['user__icontains',]
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ('status',)
+    search_fields = ['title__icontains', 'content__icontains']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name__icontains',]
+    prepopulated_fields = {'slug': ('name',)}
--- sports_news_app/models.py
+++ sports_news_app/models.py
@@ -1,12 +1,52 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name        = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    # article = models.ForeignKey(
+    #     Article,
+    #     null=True,
+    #     on_delete=models.CASCADE,
+    #     related_name='tags',
+    #     related_query_name='tag',
+    # )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name        = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+STATUS = (
+    (0, "Черновик"),
+    (1, "Опубликовано")
+)
 
 class Article(models.Model):
-    title   = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-
-    # TODO if I need to edit `created`, use this:
-    # created = models.DateTimeField(default=timezone.now),
+    title      = models.CharField(max_length=200, unique=True, null=True)
+    slug       = models.SlugField(max_length=200, unique=True, null=True)
+    author     = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='articles')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content    = models.TextField(null=True)
+    status     = models.IntegerField(choices=STATUS, default=0)
+    tags       = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
@@ -14,3 +54,5 @@ class Article(models.Model):
     class Meta:
         verbose_name        = 'Статья'
         verbose_name_plural = 'Статьи'
+
+# TODO News, User, Comment
