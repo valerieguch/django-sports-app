b28c06632e8ecf546209944543d22373e9de7d32
Valerieguch <valeria.guch2000@gmail.com>
2022-12-15 15:33:56 +0300

    Redirect to a different URL after login.

--- website/settings.py
+++ website/settings.py
@@ -128,6 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# TODO maybe redirect to a different URL?
+LOGIN_REDIRECT_URL = '/api/'
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
