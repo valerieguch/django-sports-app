c157f5a53e7923130dbb18afb6fcbda59acf3ec7
Valerieguch <valeria.guch2000@gmail.com>
2023-02-12 00:48:46 +0300

    feat: export data from Django Admin using `django-import-export`

--- sports_news_app/admin.py
+++ sports_news_app/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import Author, Tag, Article, Comment
+from import_export.admin import ExportActionModelAdmin
 
-
+# NOTE: экспорт данных из админки в Excel
 @admin.register(Author)
-class TagAdmin(admin.ModelAdmin):
+class AuthorAdmin(ExportActionModelAdmin):
     search_fields = ['user__icontains',]
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(ExportActionModelAdmin):
     list_display = ('title', 'tags_list', 'status','created_on')
     list_filter = ('status',)
     search_fields = ['title__icontains', 'content__icontains']
@@ -19,12 +20,12 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(ExportActionModelAdmin):
     search_fields = ['name__icontains',]
     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(ExportActionModelAdmin):
     list_display = ('author', 'body', 'article')
     search_fields = ['body__icontains',]
--- website/settings.py
+++ website/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'sports_news_app'
+    'sports_news_app',
+    'import_export',
 ]
 
 MIDDLEWARE = [
